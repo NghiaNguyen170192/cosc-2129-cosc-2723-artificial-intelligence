7ce2ed7c42007bc21deb8df76d5126d6b34a9b7f
Nguyễn Quốc Trọng Nghĩa <nnguyen@redcross.org.au>
2025-07-08 19:41:32 +0700

    update

---
+++ assignments/assignment-01-part-0-DUE-2025-07-10/buyLotsOfFruit.py
@@ -0,0 +1,3 @@
+def buyLotsOfFruit(orderList):
+
+    return
---
+++ assignments/assignment-01-part-0-DUE-2025-07-10/shopSmart.py
@@ -0,0 +1,4 @@
+def add(a, b):
+    "Return the sum of a and b"
+    print("Passed a = %s and b = %s, returning a + b = %s" % (a, b, a + b))
+    return a + b
